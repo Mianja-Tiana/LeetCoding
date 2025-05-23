69ad20bc0e1e5b883ae09e64d009418f53bbf949
Mianja-Tiana <arabemanantsoa@aimsammi.org>
2025-05-23 16:39:23 +0000

    Second problem

--- leetcode.py
+++ leetcode.py
@@ -10,3 +10,15 @@ class LeetCode:
             """
             x_str = str(x)
             return x_str == x_str[::-1]
+    
+# Two Sum Problem :https://leetcode.com/problems/two-sum/
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i , num1 in enumerate (nums):
+            for j ,num2 in enumerate (nums):
+                if i != j and num1 + num2 == target:
+                    return [i, j]
--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from leetcode import LeetCode
 
 if __name__ == "__main__":
     leet_obj = LeetCode()
+
+    print ("----------- Palindrome solution ----------")
+
+    print (" ")
     print(leet_obj.isPalindrome(121))   # True
     print(leet_obj.isPalindrome(-121))  # False
     print(leet_obj.isPalindrome(10))  # False
+    print (" ")
+
+    print ("----------- TwoSum solution ----------")
+    print (" ")
+    print(leet_obj.twoSum([3,2,4],6))   
+    print(leet_obj.twoSum([2,7,11,15],9))  
+    print(leet_obj.twoSum([3,3],6))
+    print (" ")
+      
