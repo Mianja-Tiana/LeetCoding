e79d2640e4cbf103112f05d974fad25c16a5d9e7
Mianja-Tiana <arabemanantsoa@aimsammi.org>
2025-05-23 15:13:28 +0000

    First problem

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,12 @@
 """
 """
 class LeetCode:
-    pass
+
+#Palindrome Number : https://leetcode.com/problems/palindrome-number/description/
+    def isPalindrome(self, x):
+            """
+            :type x: int
+            :rtype: bool
+            """
+            x_str = str(x)
+            return x_str == x_str[::-1]
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from leetcode import LeetCode
 
 if __name__ == "__main__":
     leet_obj = LeetCode()
+    print(leet_obj.isPalindrome(121))   # True
+    print(leet_obj.isPalindrome(-121))  # False
+    print(leet_obj.isPalindrome(10))  # False
