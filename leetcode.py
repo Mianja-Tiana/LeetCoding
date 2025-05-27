2408073af36a776134b0118cd37d6ac3ae96f18d
Mianja-Tiana <arabemanantsoa@aimsammi.org>
2025-05-27 21:29:46 +0000

    6th Problem

--- leetcode.py
+++ leetcode.py
@@ -1,34 +1,115 @@
 """
 """
+
+
+class ListNode(object):
+     
+    def __init__(self, val= 0, next=None):
+         self.val = val
+         self.next = next
+
+    @staticmethod
+    def from_list(input_list):
+        if len(input_list)==0:
+            return None
+
+        first = ListNode(val = input_list[0])
+        current = first 
+
+        for i in range(1,len(input_list)):
+            current.next= ListNode(val = input_list[i])
+            current = current.next
+        return first
+    
+    def __str__(self):
+        output = "["
+        current = self
+        while current :
+            output+=str(current.val)
+            output +=","
+            current = current.next
+        output += ']'
+        return output
+
+
 class LeetCode:
 
 #Palindrome Number : https://leetcode.com/problems/palindrome-number/description/
-    def isPalindrome(self, x):
+
+    def isPalindrome_slicing(self, x:int)->bool:
+            
             """
-            :type x: int
-            :rtype: bool
+            Checks if the input number is a palindrome.
+            (A numeric palindrome is a number that remains the same when its digits are reversed.)
+
+            Args:
+                 x (int): An integer number.
+
+            Returns:
+                 bool: True if the number is a palindrome, False otherwise.
             """
+
+            #Using slicing
             x_str = str(x)
             return x_str == x_str[::-1]
     
+    def isPalindrome_loop(self, x:int)->bool:
+            
+            """
+            Checks if the input number is a palindrome.
+            (A numeric palindrome is a number that remains the same when its digits are reversed.)
+
+            Args:
+                 x (int): An integer number.
+
+            Returns:
+                 bool: True if the number is a palindrome, False otherwise.
+            """
+
+            # Without slicing
+            x_str = str(x)
+            n = len(x_str)
+            for i in range(n//2):
+                if x_str[i] !=x_str[n-1-i]:
+                    return False
+            return True
+    
 # Two Sum Problem :https://leetcode.com/problems/two-sum/
-    def twoSum(self, nums, target):
+
+    def twoSum(self, nums:list[int], target:int) -> list[int]:
+
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        Given an array of integers and a target value, find the indices of the two numbers
+        that add up to the target, using each element only once.
+
+        Args:
+            nums (list[int]) : list of integers
+            target (int) : the target sum
+
+        Returns:
+            list [int]: Indices of the two numbers whose sum equals the target.
         """
-        for i , num1 in enumerate (nums):
-            for j ,num2 in enumerate (nums):
-                if i != j and num1 + num2 == target:
-                    return [i, j]
+
+        n = len(nums)
+        for idx1 in range(n):
+            for idx2 in range(idx1+1,n):
+                if nums[idx1] + nums[idx2] == target:
+                    return [idx1, idx2]
                 
 # Roman to Integer : https://leetcode.com/problems/roman-to-integer/
-    def romanToInt(self, s):
+
+    def romanToInt(self, s:str) -> int:
+
         """
-        :type s: str
-        :rtype: int
+        Converts a Roman numeral string to an integer.
+
+        Args:
+            s (str): A string representing the Roman numeral.
+        
+        Returns:
+            int: The corresponding integer value.
         """
+
         roman = {
             'I': 1,
             'V': 5,
@@ -37,25 +118,34 @@ class LeetCode:
             'C': 100,
             'D': 500,
             'M': 1000
-}
+            }
 
         total = 0
-        prev_value =0
-        for char in reversed(s):
-            current = roman[char]
-            if current < prev_value:
-                total -= current
-            else :
-                total += current
-            prev_value = current
+        n = len(s)
+
+        for i in range(n):
+            current = roman[s[i]]
+            if i + 1< n and roman[s[i+1]] > current:
+                total -=current
+            else:
+                total +=current
         return total
+
     
     
 #Longest Common Prefix :https://leetcode.com/problems/longest-common-prefix/description/
-    def longestCommonPrefix(self, strs):
+
+    def longestCommonPrefix(self, strs:list[str])-> str:
+
         """
-        :type strs: List[str]
-        :rtype: str
+        Returns the longest common prefix string among an array of strings.
+        If there is no common prefix, returns an empty string
+
+        Args :
+            strs (list[str]): A list of strings.
+
+        Returns:
+            str: The longest common prefix shared by all strings in the list.
         """
         if not strs:
             return ""
@@ -67,11 +157,23 @@ class LeetCode:
                     return strs[0][:i]
         
         return strs[0]
+    
 #Valid Parentheses : https://leetcode.com/problems/valid-parentheses/description/
-    def isValid(self, s):
+    def isValid(self, s:str)-> bool:
         """
-        :type s: str
-        :rtype: bool
+        Checks whether a string containing only brackets is valid.
+
+        A valid string requires:
+        - Each opening bracket to be closed by the same type of bracket.
+        - Brackets to be closed in the correct order.
+        - Every closing bracket to have a matching opening bracket.
+
+        Args:
+            s (str): A string containing only the characters '(', ')', '{', '}', '[' and ']'.
+
+        Returns:
+            bool : True if the string is valid, False otherwise.
+        
         """
         brackets =[]
         mapping = {')':'(',
@@ -88,5 +190,46 @@ class LeetCode:
             else:
                 return False
         return not brackets
+    
+
+# Merge Two Sorted Lists : https://leetcode.com/problems/merge-two-sorted-lists/description/
+# Definition for singly-linked list.
+
+    def mergeTwoLists(self, list1:list[int], list2 :list[int]) -> ListNode:
+        """
+        Merge two sorted singly-linked lists into one sorted linked list.
+
+        Each list is already sorted in ascending order. The merged list should also be sorted.
+        Nodes are reused from the input lists (no new nodes are created).
+
+        Args:
+            list1 (List[int]): first sorted list
+            list2 (List[int]): second sorted list
+
+        Returns:
+            ListNode: Head of the merged sorted linked list
+
+        """
+        first = ListNode()
+        current = first
+        list1 = ListNode.from_list(list1)
+        list2= ListNode.from_list(list2)
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
+        return first.next 
+        
+
             
             
